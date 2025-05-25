ba560970c8c75311d5d538c6e0eeb544211a9d98
Benie <beniedav@gmail.com>
2025-05-26 00:21:03 +0200

    added piping logic

--- pipex.c
+++ pipex.c
@@ -22,10 +22,49 @@ int	main(int ac, char **av, char **envp)
 	init_struct(&pipex, av, envp);
 	ft_printf("Struct initialized successfully\n");
 
-	char *argv[] = {pipex.cmds1[0], pipex.cmds1[1], NULL};
+	if (pipe(pipex.pipefd) == -1)
+    		error_and_exit("Pipe");
 
-	execve(pipex.cmds1[0], argv, envp);
+	pipex.pid1 = fork();
+	if (pipex.pid1 < 0)
+		error_and_exit("fork");
+	if (pipex.pid1 == 0) // CHILD 1
+	{
+ 	  	// Redirect stdin from infile	
+		dup2(pipex.infile_fd, STDIN_FILENO);
 
+    		// Redirect stdout to pipe write end
+	  	dup2(pipex.pipefd[1], STDOUT_FILENO);
+
+		// Close unused fds
+		close(pipex.pipefd[0]); // don’t read from pipe
+		close(pipex.pipefd[1]); // already duped
+		close(pipex.infile_fd);
+	   	close(pipex.outfile_fd);
+    		// execve
+		execve(pipex.cmds1[0], pipex.cmds1, envp);
+		error_and_exit("Command1 execve failed");
+	}
+	pipex.pid2 = fork();
+	if (pipex.pid2 < 0)
+		error_and_exit("fork");
+	if (pipex.pid2 == 0) // CHILD 1
+	{
+ 	  	// Redirect stdin from pipe read end
+		dup2(pipex.pipefd[0], STDIN_FILENO);
+
+    		// Redirect stdout to oufile 
+	  	dup2(pipex.outfile_fd, STDOUT_FILENO);
+
+		// Close unused fds
+		close(pipex.pipefd[1]); // don’t read from pipe
+		close(pipex.pipefd[0]); // already duped
+		close(pipex.infile_fd);
+	   	close(pipex.outfile_fd);
+    		// execve
+		execve(pipex.cmds2[0], pipex.cmds2, envp);
+		error_and_exit("Command2 execve failed");
+	}
 	free_arr(pipex.cmd_paths);
 	free_arr(pipex.cmds1);
 	free_arr(pipex.cmds2);
