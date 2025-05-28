5b5166244c92657fb609af7799b564a9a8de14c7
Benie Adou <badou@cbr11s6.42barcelona.com>
2025-05-28 18:28:50 +0200

    :(

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: badou <badou@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 16:14:51 by badou             #+#    #+#             */
-/*   Updated: 2025/05/26 16:54:27 by badou            ###   ########.fr       */
+/*   Updated: 2025/05/28 18:19:35 by badou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 int	main(int ac, char **av, char **envp)
 {
 	t_pipex	pipex;
+	int		status;
 
 	if (ac != 5)
 		custom_exit("Expected 4 arguments", 1);
 	init_struct(&pipex, av, envp);
 	exec_processes(&pipex, envp);
 	waitpid(pipex.pid1, NULL, 0);
-	waitpid(pipex.pid2, NULL, 0);
+	waitpid(pipex.pid2, &status, 0);
 	cleanup(&pipex);
-	return (0);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (1);
 }
